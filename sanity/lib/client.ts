77a054bca96e7f06107b39e64c2cbb0dab207c8a
Eray Demircioglu <c.eray@hotmail.com>
2025-02-12 15:43:39 +0300

    LMS Platform

---
+++ sanity/lib/client.ts
@@ -0,0 +1,14 @@
+import { createClient } from "next-sanity";
+
+import { apiVersion, dataset, projectId } from "../env";
+import baseUrl from "@/lib/baseUrl";
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true,
+  stega: {
+    studioUrl: `${baseUrl}/studio`,
+  },
+});
---
+++ sanity/lib/lessons/uncompleteLessonById.ts
@@ -0,0 +1,26 @@
+import { client } from "../adminClient";
+import { sanityFetch } from "../live";
+import groq from "groq";
+
+interface UncompleteLessonParams {
+  lessonId: string;
+  clerkId: string;
+}
+export async function uncompleteLessonById({
+  lessonId,
+  clerkId,
+}: UncompleteLessonParams) {
+  const student = await sanityFetch({
+    query: groq`*[_type == "student" && clerkId == $clerkId][0]._id`,
+    params: { clerkId },
+  });
+
+  if (!student.data) {
+    throw new Error("Student not found");
+  }
+
+  await client.delete({
+    query: `*[_type == "lessonCompletion" && student._ref == $studentId && lesson._ref == $lessonId][0]`,
+    params: { studentId: student.data, lessonId },
+  });
+}
